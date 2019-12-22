724fbfe20a97c1091afb3f6bc844877f39c4e10f
Kyle Fang <zhigang1992@gmail.com>
2019-12-22 13:23:09 +0800

    feat: add spinner

---
+++ app/src/components/Loader.tsx
@@ -0,0 +1,17 @@
+import "./Loader.css";
+import React from "react";
+
+const Loader = () => (
+  <div className="lds-roller self-center">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
--- app/src/components/RightSideBar.tsx
+++ app/src/components/RightSideBar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Loader from "./Loader";
 
 const RightSideBar = () => (
   <div className="flex flex-shrink-0 flex-col w-40 ml-5 pt-5">
     <button className="bg-blue-500 h-10 rounded font-bold text-white">
       Generate Schema
     </button>
+    <Loader />
   </div>
 );
 
